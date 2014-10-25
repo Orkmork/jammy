425935de0a267033cbeac979a747a87303afd12c
Orkmork <simon.perkampus@gmail.com>
2014-10-25 20:19:58 +0200

    Many fucking stuff

---
+++ mainproject/Assets/Scripts/PowerupScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupScript : MonoBehaviour {
+
+	CharacterControllerScript fiona;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			fiona = GameObject.Find ("Character").GetComponent<CharacterControllerScript>();
+			fiona.anim.SetBool ("HasWeapon",true);
+			Destroy (this.gameObject);
+		}
+	}
+}
