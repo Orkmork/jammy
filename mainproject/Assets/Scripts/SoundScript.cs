a6e4e7394da3a89b1dad1714f76066f6ff52d75d
Orkmork <simon.perkampus@gmail.com>
2014-10-25 11:34:56 +0200

    my stuff

---
+++ mainproject/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundScript : MonoBehaviour {
+
+	public AudioClip kick;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftControl)) {
+			if(!audio.isPlaying) {
+				audio.clip = kick;
+				audio.Play ();
+			}
+		}
+	}
+
+	void SoundEnd() {
+	}
+}
