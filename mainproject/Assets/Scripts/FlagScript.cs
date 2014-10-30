83d073d4168ced31e438c14bff84d5080502aca7
Orkmork <simon.perkampus@gmail.com>
2014-10-30 23:50:25 +0100

    Adding Spawnpoints
    - work in progress: letzte Spawnpunkt Aktivierung speichern
    - nochmal checken, ob das so auch Sinn macht (doofe Schmerzen)

---
+++ mainproject/Assets/Scripts/FlagScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlagScript : MonoBehaviour {
+
+	public bool isStartSpawn = true;
+	public Animator anim;
+	CharacterControllerScript fiona;
+	SoundScript sfx;
+
+	FlagScript() {
+
+	}
+
+	void Awake() {
+		//transform.FindChild ("flag_grey").gameObject.SetActive (false);
+		anim = GetComponent<Animator> ();
+		if (isStartSpawn) {
+			anim.SetBool ("Startspawn", true);
+		}
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		/*if (isActive) {
+			anim.SetBool ("Active", true);
+		} else {
+			anim.SetBool ("Active", false);
+		}*/
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player" && !isStartSpawn)
+		{
+			fiona = GameObject.Find ("Character").GetComponent<CharacterControllerScript>();
+			sfx = fiona.GetComponent<SoundScript>();
+			sfx.playBox();
+			anim.SetBool ("Active", true);
+			BoxCollider2D[] myColliders = gameObject.GetComponents<BoxCollider2D>();
+			foreach(BoxCollider2D bc in myColliders) bc.enabled = false;
+
+		}
+	}
+}
