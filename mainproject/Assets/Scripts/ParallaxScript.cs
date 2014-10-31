53e4c07db187b4b0e181fa8d088bff493a3778b1
sig404 <sigi.kullmann@googlemail.com>
2014-10-31 10:49:41 +0100

    - added test scene Zoo_1

---
+++ mainproject/Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScript : MonoBehaviour {
+
+	public Transform mainCamera;
+	public float speed = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 cam = mainCamera.position;
+		
+		renderer.material.mainTextureOffset = new Vector2(
+			(cam.x * speed) % 1.0f,
+			(cam.y * speed) % 1.0f
+		);
+	}
+}
