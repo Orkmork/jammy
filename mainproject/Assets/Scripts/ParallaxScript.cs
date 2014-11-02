2f5852ca1fcb5b392d31f482f8f8d33768e5bccc
sig404 <sigi.kullmann@googlemail.com>
2014-11-02 20:23:22 +0100

    - levels fixed

--- mainproject/Assets/Scripts/ParallaxScript.cs
+++ mainproject/Assets/Scripts/ParallaxScript.cs
@@ -3,16 +3,43 @@ using System.Collections;
 
 public class ParallaxScript : MonoBehaviour {
 
+	public Transform level;
 	public Transform mainCamera;
-	public float speed = 0;
+	public Vector2 offset = new Vector2(0, 0);
+	public Vector2 speed = new Vector2(0, 0);
+	public bool doWrapX = true;
+	public bool doWrapY = false;
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 cam = mainCamera.position;
-		
-		renderer.material.mainTextureOffset = new Vector2(
-			(cam.x * speed) % 1.0f,
-			(cam.y * speed) % 1.0f
+		Vector2 cam = new Vector2(
+			mainCamera.position.x,
+			mainCamera.position.y
 		);
+		
+		if (level != null) {
+			cam.x -= level.position.x;
+			cam.y -= level.position.y;
+		}
+		
+		Vector2 ofs = new Vector2 (
+			wrapOrClip(cam.x * speed.x + offset.x, doWrapX),
+			wrapOrClip(cam.y * speed.y + offset.y, doWrapY)
+		);
+
+		renderer.material.mainTextureOffset = ofs;
+	}
+	
+	float wrapOrClip(float x, bool doWrap) {
+		if (doWrap) {
+			x %= 1.0f;
+		}
+		else if (x < 0) {
+			x = 0;
+		}
+		else if (x > 0.9999f) {
+			x = 1.0f;
+		}
+		return x;
 	}
 }
